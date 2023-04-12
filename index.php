8289336ff170e529b9d9433301d6250ff96aef88
Ricky Raven <rickyraven123@gmail.com>
2023-04-12 21:45:27 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eaque reprehenderit, vel quam inventore ducimus tempora sunt officiis mollitia praesentium dicta quasi magnam, placeat similique aliquid possimus id veritatis totam?</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
